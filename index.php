11012355db05b467d7cd6bf56b693f13b2c1d193
zthomaz <33027744+zthomaz@users.noreply.github.com>
2017-12-26 12:17:50 +0700

    file index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+echo "<b>";
+echo "<center><h1>Tugas Teori TCC 12 - Docker Compose</h1></center>";
+echo "<br>";
+echo "<h3>Nama 		: Thomas Prayudhi Triutomo<br>";
+echo "NIM 			: 155410041<br>";
+echo "Jurusan 		: Teknik Informatika<br>";
+echo "Github		: github.com/zthomaz</h3>";
+echo "</b>";
+?>
